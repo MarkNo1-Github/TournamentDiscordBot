6c267cb58fc367c0d91c35f16e66e9a1b6b3b1c0
Mark <markno1.github@gmail.com>
2020-06-28 21:08:45 +0200

    Logger + Data Refactoring: upcoming v2

--- tdbm/logger/logger.py
+++ tdbm/logger/logger.py
@@ -1,9 +1,72 @@
-
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from logging import Formatter
+from pathlib import Path
 from datetime import date
 import os
 
 
-__version__ = '0.0.5'
+__version__ = '0.1.7'
+
+FORMATV2 = '''\
+(%(levelname)s) %(asctime)s - pid:%(process)d\n\t%(name)s::%(filename)s:%(lineno)s\n\t%(funcName)s: %(message)s
+'''
+FORMAT = '(%(levelname)} s%(asctime)s - pid:%(process)d\n\t%(name)s::%(filename)s:%(lineno)s\n\t%(funcName)s: %(message)s'
+
+# Every 1 Day
+ROTATION = 'd'
+
+Success = lambda x : x
+Error = lambda y : y
+
+
+class EnumLogger(object):
+    DEBUG = 0
+    INFO = 1
+    WARN = 2
+    ERROR = 3
+
+
+class Logger(EnumLogger):
+    def __init__(self, name, level=logging.DEBUG):
+        self.folder = Path(name)
+
+        # Create Folder if not exist
+        if not self.folder.exists():
+            self.folder.mkdir()
+
+        # File Log
+        self.file_log = Path(name) / f'{name}.{date.today()}.log'
+
+        # Logger
+        self.logger = logging.getLogger(name)
+
+        # Handler
+        handler = logging.handlers.TimedRotatingFileHandler(self.file_log, when= ROTATION, backupCount=5)
+        handler.setFormatter(Formatter(FORMATV2))
+        self.logger.addHandler(handler)
+
+        # Set Level
+        self.logger.setLevel(level)
+
+        self.callback = {
+            self.DEBUG : lambda x : self.logger.debug(x),
+            self.INFO : lambda x : self.logger.info(x),
+            self.WARN : lambda x : self.logger.warning(x),
+            self.ERROR : lambda x : self.logger.error(x),
+        }
+
+
+    def __call__(self, level: EnumLogger, *args, **kargs):
+        if args:
+            self.callback [level] (*args)
+        if kargs:
+            self.callback [level] (kargs)
+
+
+
+
+
 
 
 def GetFileLogger(folder, name):
@@ -21,8 +84,11 @@ def GetFileLogger(folder, name):
     logger.setLevel(logging.DEBUG)
     return logger
 
-def Success(txt):
-    return "**`SUCCESS`** " + str(txt)
 
-def Error(txt):
-    return "**`ERROR`** " + str(txt)
+if __name__ == '__main__':
+    print("** Testing Logger Class **")
+    log = Logger('test_logger')
+    log(log.DEBUG, 'debug')
+    log(log.INFO, 'info')
+    log(log.WARN, 'warn')
+    log(log.ERROR, 'error')
